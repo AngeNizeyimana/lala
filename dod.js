78a4649ec5be5f5c7039e9b9b407a3994ea8537d
AngeNizeyimana <95916290+AngeNizeyimana@users.noreply.github.com>
2021-12-16 09:11:19 +0800

    My second wed site

---
+++ dod.js
@@ -0,0 +1,1 @@
+document.write("<h1>English intensifiers!</h1>")
---
+++ java.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+
+</head>
+<body>
+<script type="text/javascript">
+
+	document.write("this java script link")
+</script>
+</body>
+</html>
